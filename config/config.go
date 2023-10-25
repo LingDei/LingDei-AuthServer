8baa3ad42b08d793415abe377fcd5ac5f9300881
zjy2414 <zjy2414@outlook.com>
2023-10-26 00:42:30 +0800

    Revert "update: config.go"
    This reverts commit 947fdb6c8b166b7ae99a3c22b020ad68cdff72c6.

--- config/config.go
+++ config/config.go
@@ -9,11 +9,10 @@ var (
 	//私钥路径
 	PrivateKeyPath = "./config/rsa_private_key.pem"
 
-	// DSN
 	DSN = "lingdei:lingdei666666@tcp(127.0.0.1:3306)/LingDei?charset=utf8mb4&parseTime=True&loc=Local"
 
 	// QiNiu
-	QINIU_ACCESS_KEY = "R1weO56kS2kZK6BWEelAGaEXirAXKPAOjZ_OoYqk"
-	QINIU_SECRET_KEY = "6igpxhFmCVjjWALG8xfxi_MfSHQKkXcHlBS31wgA"
-	QINIU_BUCKET     = "lingdei"
+	QINIU_ACCESS_KEY = "your access key"
+	QINIU_SECRET_KEY = "your secret key"
+	QINIU_BUCKET     = "your bucket"
 )
