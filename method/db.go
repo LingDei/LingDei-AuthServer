d53b32c1fc83891c0c748e951a1d1af0c0f1f900
zjy2414 <zjy2414@outlook.com>
2023-10-25 10:06:19 +0800

    add: methods

---
+++ method/db.go
@@ -0,0 +1,28 @@
+package method
+
+import (
+	"LingDei_AuthServer/config"
+	"LingDei_AuthServer/model"
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func getDB() (*gorm.DB, error) {
+	db, err := gorm.Open(mysql.Open(config.DSN), &gorm.Config{})
+	return db, err
+}
+
+func init() {
+	db, err := getDB()
+	sqlDB, _ := db.DB() //结束后关闭 DB
+	defer sqlDB.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// 迁移 schema
+	db.AutoMigrate(&model.User{})
+	db.AutoMigrate(&model.Profile{})
+}
