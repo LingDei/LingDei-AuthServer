947fdb6c8b166b7ae99a3c22b020ad68cdff72c6
zjy2414 <zjy2414@outlook.com>
2023-10-25 11:01:14 +0800

    update: config.go

--- config/config.go
+++ config/config.go
@@ -4,5 +4,11 @@ var (
 	//私钥路径
 	PrivateKeyPath = "./config/rsa_private_key.pem"
 
+	// DSN
 	DSN = "lingdei:lingdei666666@tcp(127.0.0.1:3306)/LingDei?charset=utf8mb4&parseTime=True&loc=Local"
+
+	// QiNiu
+	QINIU_ACCESS_KEY = "your access key"
+	QINIU_SECRET_KEY = "your secret key"
+	QINIU_BUCKET     = "your bucket"
 )
