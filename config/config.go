baba76f54a31524b58c5cbc1f7a3fdd0fec9e569
zjy2414 <zjy2414@outlook.com>
2023-10-25 09:59:41 +0800

    add: config.go

---
+++ config/config.go
@@ -0,0 +1,8 @@
+package config
+
+var (
+	//私钥路径
+	PrivateKeyPath = "./config/rsa_private_key.pem"
+
+	DSN = "lingdei:lingdei666666@tcp(127.0.0.1:3306)/LingDei?charset=utf8mb4&parseTime=True&loc=Local"
+)
