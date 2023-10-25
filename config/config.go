8265ac43bf4a5140c0c7079624cd4c1eb5ee0244
zjy2414 <zjy2414@outlook.com>
2023-10-25 11:15:42 +0800

    update: 增加权限控制

--- config/config.go
+++ config/config.go
@@ -1,6 +1,11 @@
 package config
 
 var (
+	// 权限控制
+	UserROLE_LEVEL        = 0
+	Admin_ROLE_LEVEL      = 7
+	SuperAdmin_ROLE_LEVEL = 9
+
 	//私钥路径
 	PrivateKeyPath = "./config/rsa_private_key.pem"
 
@@ -8,7 +13,7 @@ var (
 	DSN = "lingdei:lingdei666666@tcp(127.0.0.1:3306)/LingDei?charset=utf8mb4&parseTime=True&loc=Local"
 
 	// QiNiu
-	QINIU_ACCESS_KEY = "your access key"
-	QINIU_SECRET_KEY = "your secret key"
+	QINIU_ACCESS_KEY = "R1weO56kS2kZK6BWEelAGaEXirAXKPAOjZ_OoYqk"
+	QINIU_SECRET_KEY = "6igpxhFmCVjjWALG8xfxi_MfSHQKkXcHlBS31wgA"
 	QINIU_BUCKET     = "your bucket"
 )
