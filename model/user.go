6a28c6e360706a6c8cc60bfef1b893544d301dea
zjy2414 <zjy2414@outlook.com>
2023-10-28 23:59:15 +0800

    update: 增加profile外键约束

--- model/user.go
+++ model/user.go
@@ -5,4 +5,7 @@ type User struct {
 	UserName string `json:"username" validate:"required,min=3,max=20" gorm:"not null; unique"`
 	Password string `json:"-" validate:"required,min=6,max=20" gorm:"not null"`
 	Role     int    `json:"role" gorm:"not null; default:0"`
+
+	// Profile
+	Profile Profile `json:"profile" validate:"-" gorm:"foreignKey:ID; references:ID; constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
